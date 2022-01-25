27903dfc383ba454b2f5e3e70cf5754fa79875f3
Luana Matias Terense <luana.r.matias@gmail.com>
2022-01-24 21:36:52 -0300

    Delete Pet

--- cypress/integration/DELETE_PET.spec.js
+++ cypress/integration/DELETE_PET.spec.js
@@ -11,6 +11,7 @@ it('POST por arquivo JSON: Deve realizar a inclusão do pet de código 261291', 
 })
 
 it('Deve realizar a exclusão do pet de código 261291', () => {
+    cy.wait(3000) //A API possui lentidão para processos, então é necessário forçar esse tempo
     cy.request({
         method: 'DELETE',
         url: 'https://petstore.swagger.io/v2/pet/261291',
@@ -20,15 +21,12 @@ it('Deve realizar a exclusão do pet de código 261291', () => {
 })
 
 it('Garantir que a exclusão foi feita realmente', () => {
-    
-    cy.wait(2000)
+    cy.wait(8000) //A API possui lentidão para processos, então é necessário forçar esse tempo
     cy.request({
         method: 'GET',
         url: 'https://petstore.swagger.io/v2/pet/261291',
         failOnStatusCode: false,
     }).then(res => {
-        //expect(res.status).to.be.equal(404)
-        expect(res.message).to.eq('Pet not found')
-
+        expect(res.body.message).to.eq('Pet not found') //A API não é estável, então pode falhar nessa linha.
     })
 })
